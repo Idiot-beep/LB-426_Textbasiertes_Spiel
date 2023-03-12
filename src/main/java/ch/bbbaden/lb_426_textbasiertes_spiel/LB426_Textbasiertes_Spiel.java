cdba379042d469d09d40e51bf59c36e7eceaa4de
Rouven Butz <R.Butz.inf20@stud.bbbaden.ch>
2023-03-12 09:12:49 +0100

    v 0.1 Erste Version des Projekts

---
+++ src/main/java/ch/bbbaden/lb_426_textbasiertes_spiel/LB426_Textbasiertes_Spiel.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ch.bbbaden.lb_426_textbasiertes_spiel;
+
+/**
+ *
+ * @author rouve
+ */
+public class LB426_Textbasiertes_Spiel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
